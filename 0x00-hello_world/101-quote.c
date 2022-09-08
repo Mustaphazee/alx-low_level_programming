bd5972ab548875552a533ca4b47a7ead792184ea
mustapha zainab <zainab.tundemustapha06@gmail.com>
2022-09-08 16:29:37 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - print the string in the put function 
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpor, 2015-10-19\n" 59);
+	return(1);
+}
