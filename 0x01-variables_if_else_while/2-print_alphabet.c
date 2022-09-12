ef213243ff4f22c70e00393edd83a8504c43d5fa
mustapha zainab <zainab.tundemustapha06@gmail.com>
2022-09-12 03:22:06 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print if the number is positive, zero, or negative
+ * main - Entry point
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
