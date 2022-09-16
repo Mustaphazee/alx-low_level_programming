7f79b0f4d574a332044feead88b6f12206034830
mustapha zainab <zainab.tundemustapha06@gmail.com>
2022-09-16 15:00:52 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,17 +11,20 @@ void print_square(int size)
 {
 	int inc1, inc2;
 
-	if (size <= 0)
-	    _putchar('\n');
-	else
+	if (size > 0)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
-				putchar(35);
+				putchar('#');
 			}
-
-			_putchar('\n');
+                        putchar('#');
+			putchar('\n');
 		}
-	}	
+	}
+	else
+	{
+		putchar('\n');
+	}
+}	
