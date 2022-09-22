c34b344cd9b81efbbfd133ac08e1fbd23bb191f9
mustapha zainab <zainab.tundemustapha06@gmail.com>
2022-09-22 14:04:10 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
  * string_toupper - chamges all lowercase letters to uppercase
  * @str: string to be changed
  *
- * Return: address to the string
+ * Return: address tp the string
  */
 
 char *string_toupper(char *str)
